926f329e4d4bf66833aa8603e1547deef762c489
Aleksei Faians <af.git@bsrealm.net>
2022-08-17 15:12:48 +0300

    Cosmetic fix

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   folder_id      = "b1g54p9r1chtg9iju7ca"
   network_id     = "enp305qhkqk2ekira1mo"
   availability_zones = [
-    { "zone" : "ru-central1-a", "subnet" : "e9bhp1sm4upq328p2jm4" },
-    { "zone" : "ru-central1-b", "subnet" : "e2l0bo5a4q5ru8vc7k7u" }
+    { "zone" = "ru-central1-a", "subnet" = "e9bhp1sm4upq328p2jm4" },
+    { "zone" = "ru-central1-b", "subnet" = "e2l0bo5a4q5ru8vc7k7u" }
   ]
 }
